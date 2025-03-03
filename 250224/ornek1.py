09a0bf647ed66152893f1d55b985c7807ceab164
nazelif1053 <elifnazkizilkaya45@gmail.com>
2025-03-03 11:17:18 +0300

    Add files via upload

---
+++ 250224/ornek1.py
@@ -0,0 +1,5 @@
+yas=int(input("yaşınızı girin:"))
+if yas>=18:
+    print("ehliyet alabilirsiniz")
+else:
+    print("ehliyet alamazsınız")
---
+++ 250224/ornek2.py
@@ -0,0 +1,5 @@
+havadurumu=int(input("hava sıcaklığı:"))
+if havadurumu>=20:
+    print("hava sıcak")
+else:
+    print("hava soğuk")
